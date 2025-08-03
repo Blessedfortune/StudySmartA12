f084e6fe312b4d58e22ca7ae18c8ef4bcd3e6f10
Blessedfortune <lokwavij@gmail.com>
2025-08-03 00:53:31 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias("com.google.gmsgoogle-services")
 }
 
 android {
